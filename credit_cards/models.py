f541d0728c6487510256bb21f8aba93281b67db1
victorpajeu <pajeuvictor@gmail.com>
2023-11-25 19:40:13 -0300

    feat: credit card model creation

--- credit_cards/models.py
+++ credit_cards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django_cryptography.fields import encrypt
 
-# Create your models here.
+
+class CreditCard(models.Model):
+    holder = models.CharField("Card Holder", max_length=50)
+    number = encrypt(models.CharField("Card Number", max_length=20))
+    cvv = models.IntegerField("CVV", blank=True, null=True)
+    brand = models.CharField("Card Brand", max_length=20, blank=True, null=True)
+    exp_date = models.DateField("Expiration Date")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        try:
+            decrypted_last_four = self.number[-4:]
+        except Exception as e:
+            decrypted_last_four = "****"
+        return f"{self.holder}'s Credit Card ending in {decrypted_last_four}"
