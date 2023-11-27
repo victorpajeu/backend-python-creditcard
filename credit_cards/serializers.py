1781779a6f30ba7781fa6d3b27ed6f585a9d04d8
victorpajeu <pajeuvictor@gmail.com>
2023-11-26 23:30:34 -0300

    fix: serializer inheriting from Serializer Class, facilitating evaluations, and using the create method to validate the data

--- credit_cards/serializers.py
+++ credit_cards/serializers.py
@@ -1,21 +1,25 @@
+import calendar
+from datetime import datetime
 from rest_framework import serializers
-from django_cryptography.fields import encrypt
+
 
 # Models
 from creditcard import CreditCard
 
-class CreditCardSerializer(serializers.ModelSerializer):
+class CreditCardSerializer(serializers.Serializer):
     holder = serializers.CharField(min_length=3)
     number = serializers.CharField()
     cvv = serializers.IntegerField(min_value=100, max_value=9999)
     brand = serializers.CharField(read_only=True)
     exp_date = serializers.DateField(format='%m/%Y', input_formats=['%m/%Y'])
 
-    class Meta:
-        model = CreditCard
-        fields = ('holder', 'number', 'cvv', 'brand', 'exp_date')
-
-    def validate_number(self, value):
-        if not CreditCard(value).is_valid():
-            raise serializers.ValidationError("Invalid credit card number.")
-        return encrypt(value)
+    def create(self, data):
+        cc = CreditCard(data.get('number'))
+        if cc.is_valid():
+            data['brand'] = cc.get_brand()
+            exp_date = data['exp_date']
+            if exp_date > datetime.today().date():
+                data['exp_date'] = exp_date.replace(day=calendar.monthrange(exp_date.year, exp_date.month)[1])
+                return CreditCard.objects.create(**data)
+            raise serializers.ValidationError("Invalid date")
+        raise serializers.ValidationError("Card number provided is invalid")
