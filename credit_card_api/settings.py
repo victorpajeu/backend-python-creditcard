a082dac0429e66a6dc03a48724e4914630da417a
victorpajeu <pajeuvictor@gmail.com>
2023-11-27 04:36:23 -0300

    feat: adding swagger to the project

--- credit_card_api/settings.py
+++ credit_card_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # External Apps
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- credit_card_api/urls.py
+++ credit_card_api/urls.py
@@ -15,12 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from credit_cards.urls import router
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Credit Card API",
+        default_version='v1',
+        description="Simple credit card registration API",
+        terms_of_service="http://localhost:8000/api/v1/credit-card/",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'^documentation(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('documentation/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
